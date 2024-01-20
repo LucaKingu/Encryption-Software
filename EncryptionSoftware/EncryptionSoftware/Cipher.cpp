05b71cc28651a62ce9eac7ce259741b3c7c783cf
Luca Alfino <alfinoluca21@gmail.com>
2024-01-20 15:10:08 +0100

    Added header file functions / Initilized them into source file

--- EncryptionSoftware/EncryptionSoftware/Cipher.cpp
+++ EncryptionSoftware/EncryptionSoftware/Cipher.cpp
@@ -1,1 +1,35 @@
 #include "Cipher.h"
+
+#include <aes.h>
+#include <filters.h>
+#include <modes.h>
+#include <osrng.h> //Operating system random generation used for AutoSeededRandomPool
+
+
+Cipher::Cipher(const char* password)
+{
+}
+
+Cipher::~Cipher()
+{
+}
+
+bool Cipher::encryptFile(const char* inputFileName, const char* outputFileName)
+{
+	return false;
+}
+
+bool Cipher::decryptFile(const char* inputFileName, const char* outputFileName)
+{
+	return false;
+}
+
+bool Cipher::generateRandomIV()
+{
+	return false;
+}
+
+bool Cipher::deriveKeyFromPassword(const char* password, size_t passwordLength)
+{
+	return false;
+}
--- EncryptionSoftware/EncryptionSoftware/Cipher.h
+++ EncryptionSoftware/EncryptionSoftware/Cipher.h
@@ -1,6 +1,29 @@
 #pragma once
 
-//class Cipher
-//{
-//};
+
+
+
+using namespace CryptoPP;
+
+class Cipher
+{
+public:
+	//const keywords since it is important that passwords and file names are immutable
+	Cipher(const char* password);
+	~Cipher();
+
+	bool encryptFile(const char* inputFileName, const char* outputFileName);
+	bool decryptFile(const char* inputFileName, const char* outputFileName);
+
+private:
+	char* storedPassword;
+	
+//From cryptlib library
+	SecByteBlock key;	//Class to hold cryptographic keys
+	SecByteBlock iv;	//Same class to hold an initilization vector(Random number)
+	AutoSeededRandomPool prng;  //A class to generate a hilghy secure random number for the iv
+
+	bool generateRandomIV();	
+	bool deriveKeyFromPassword(const char* password, size_t passwordLength);
+};
 
